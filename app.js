b7226296c3d59f7b896623bad73d14a74bd26a8e
Andrew Gasparovich <afgasparo@gmail.com>
2021-01-31 22:10:02 -0500

    Basic DB operations complete. Still need R operations.

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ const mongoose = require("mongoose");
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/namesDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const namesSchema = {
+  name: String,
+  timeLeft: String,
+  timeBack: String
+};
+
+const Name = mongoose.model("Name", namesSchema);
+
+
+//
+// const name1 = new Name({
+//   name: "Jeff Johnson"
+// });
+//
+// const name2 = new Name({
+//   name: "Johnny Jeffseed",
+//   timeLeft: "4:20"
+// });
+//
+// const defNames = [name1, name2];
+//
+// Name.insertMany(defNames, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved default items to namesDB");
+//   }
+// })
+
 // send index.ejs
 app.get("/", function(req, res) {
   res.render("index.ejs");
@@ -18,15 +52,53 @@ app.get("/index.js", function(req, res) {
 io.on('connection', function(socket) {
   // catch emissions for functions
   socket.on('new_name', function(data, tableIndex, sid) {
-    io.emit('new_name', "<td>" + data + "</td><td><button id='button" + tableIndex + "' class='btn btn-light'>Check Out</button></td><td id='button" + tableIndex + "row'></td><td><button id='remove" + tableIndex + "' class='btn btn-light'>X</button></td></tr>", sid);
+    const name = new Name({
+      name: data
+    });
+
+    Name.create(name, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully saved " + data + " to namesDB");
+      }
+    })
+
+    io.emit('new_name', "<td>" + data + "</td><td><button id='checkOutBtn" + tableIndex + "' class='btn btn-light'>Check Out</button></td><td></td><td><button id='remove" + tableIndex + "' class='btn btn-light'>X</button></td></tr>", sid);
   });
-  socket.on('remove_name', function(data) {
+  socket.on('remove_name', function(data, name2) {
+    Name.deleteOne({name: name2}, function(err){
+      if (!err) {
+        console.log("Successfully deleted " + name2 + " from namesDB");
+      } else {
+        console.log(err);
+      }
+    })
+
     io.emit('remove_name', data);
   });
-  socket.on('check_out', function(data,time) {
+
+  socket.on('check_out', function(data, time, name2) {
+    Name.findOneAndUpdate({name: name2}, {timeLeft: time}, {upsert: true}, function(err) {
+      if (!err) {
+        console.log("Successfully added " + time + " to time-left for " + name2 + " in namesDB");
+      } else {
+        console.log(err);
+      }
+    })
+
     io.emit('check_out', data, time);
   });
-  socket.on('check_in', function(data, time) {
+
+  socket.on('check_in', function(data, time, name2) {
+    Name.findOneAndUpdate({name: name2}, {timeBack: time}, {upsert: true}, function(err) {
+      if (!err) {
+        console.log("Successfully added " + time + " to time-back for " + name2 + " in namesDB");
+      } else {
+        console.log(err);
+      }
+    })
+
     io.emit('check_in', data, time);
   });
 });
--- index.js
+++ index.js
@@ -18,38 +18,28 @@ $('#addNameForm').submit(function(e) {
 // append the new name
 socket.on('new_name', function(data, sid) {
   $('#tableBody').append($("<tr>").html(data)); // append data to table
-  $('#tableBody tr:last').attr("id", "row" + tableIndex); // append row# id
+  $('#tableBody tr:last').prop("id", "row" + tableIndex); // append row# id
 
   // click listener for remove name button
-  $(document).on("click", "#remove" + tableIndex, function() {
-    if (confirm("Are you sure you want to remove " + $(this).parent().parent().children()[0].innerHTML + "?")) {
-      // emit the id of the remove button
-      socket.emit('remove_name', $(this).parent().parent().attr("id"));
+  $(document).on("click", "#remove" + tableIndex, function(e) {
+    if (confirm("Are you sure you want to remove " + $(this).parent().parent().children().first().html() + "?")) {
+      // emit the id of the row
+      e.preventDefault();
+      socket.emit('remove_name', $(this).parent().parent().prop("id"), $(this).parent().parent().children().first().html());
     }
   })
 
   // click listener for check-out button
-  $(document).on("click", "#button" + tableIndex, function() {
+  $(document).on("click", "#checkOutBtn" + tableIndex, function(e) {
     // get current time
     let today = new Date();
     let mins = today.getMinutes();
     if (mins.toString().length === 1)
       mins = "0" + mins;
     let time = today.getHours() + ":" + mins;
-    // emit the id of the check-out button
-    socket.emit('check_out', $(this).prop("id"), time);
-
-    // Add click listener for check-in button
-    $(document).on("click", "#" + $(this).prop("id") + "row", function() {
-      // get current time
-      let today = new Date();
-      let mins = today.getMinutes();
-      if (mins.toString().length === 1)
-        mins = "0" + mins;
-      let time = today.getHours() + ":" + mins;
-      // emit the id of the check-in button
-      socket.emit('check_in', $(this).prop("id") + "In", time);
-    })
+    // emit the id of the check-out button and time
+    e.preventDefault();
+    socket.emit('check_out', $(this).prop("id"), time, $(this).parent().parent().children().first().html());
   })
   tableIndex++;
 });
@@ -59,16 +49,28 @@ socket.on('remove_name', function(data) {
   $("#" + data).remove();
 });
 
-// time left
+// check out
 socket.on('check_out', function(data, time) {
-  $("#"+ data).before(time); // add check-out time
-  $("#"+ data).hide(); // hide the button
-  // add HTML to create check-in button (after checking out) using the cell ID created above
-  $("#" + data + "row").html("<button id='" + data + "rowIn" + "' class='btn btn-light'>Check In</button>");
+  $("#" + data).before(time); // add check-out time
+  $("#" + data).hide(); // hide the button
+  // add HTML to create check-in button (after checking out)
+  $("#" + data).parent().next().html("<button id='checkInBtn" + data.slice(-1) + "' class='btn btn-light'>Check In</button>");
 
+  // add click listener for check-in button
+  $(document).on("click", "#checkInBtn" + data.slice(-1), function(e) {
+    // get current time
+    let today = new Date();
+    let mins = today.getMinutes();
+    if (mins.toString().length === 1)
+      mins = "0" + mins;
+    let time = today.getHours() + ":" + mins;
+    // emit the id of the check-in button and time
+    e.preventDefault();
+    socket.emit('check_in', "checkInBtn" + data.slice(-1), time, $("#row" + data.slice(-1)).children().first().html());
+  })
 });
 
-// time back
+// check in
 socket.on('check_in', function(data, time) {
   $("#" + data).before(time); // add check-in time
   $("#" + data).hide(); // hide the button
