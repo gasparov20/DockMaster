e9b42b86675a92f7e8b52d3ce9507d99755fec08
Andrew Gasparovich <afgasparo@gmail.com>
2021-02-03 02:10:57 -0500

    Changed sockets.io port again again again

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ socket.on("connect", () => {
 });
 
 // reload all the times and buttons whenever the page is fully loaded
-$(window).on('load', function() {
+jQuery(window).on('load', function() {
   var x = window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart + "ms";
   console.log("page loaded in " + x);
   socket.emit('add_data');
 })
 
 // submit new table item without reload
-$('#addNameForm').submit(function(e) {
+jQuery('#addNameForm').submit(function(e) {
   e.preventDefault(); // prevents page reloading
-  socket.emit('new_name', $('#nameBox').val());
-  $('#nameBox').val('');
+  socket.emit('new_name', jQuery('#nameBox').val());
+  jQuery('#nameBox').val('');
   return false;
 });
 
 // append the new name
 socket.on('new_name', function(tableIndex, data) {
-  $('#tableBody').append($("<tr>").html(data)); // append data to table
-  $('#tableBody tr:last').prop("id", "row" + tableIndex); // append row# id
+  jQuery('#tableBody').append(jQuery("<tr>").html(data)); // append data to table
+  jQuery('#tableBody tr:last').prop("id", "row" + tableIndex); // append row# id
 
   // hide check-in button
-  $("#checkInBtn" + tableIndex).hide();
+  jQuery("#checkInBtn" + tableIndex).hide();
 
   // click listener for remove button
-  $(document).on("click", "#remove" + tableIndex, function(e) {
-    if (confirm("Are you sure you want to remove " + $("#row" + tableIndex).children().first().html() + "?")) {
+  jQuery(document).on("click", "#remove" + tableIndex, function(e) {
+    if (confirm("Are you sure you want to remove " + jQuery("#row" + tableIndex).children().first().html() + "?")) {
       // emit the id of the row
       e.preventDefault();
-      socket.emit('remove_name', tableIndex, $("#row" + tableIndex).children().first().html());
+      socket.emit('remove_name', tableIndex, jQuery("#row" + tableIndex).children().first().html());
     }
   })
   // click listener for check-out button
@@ -46,28 +46,28 @@ socket.on('new_name', function(tableIndex, data) {
 // add data
 socket.on('add_data', function (tableIndex, tL, tB) {
   // click listener for remove
-  $(document).on("click", "#remove" + tableIndex, function(e) {
-    if (confirm("Are you sure you want to remove " + $("#row" + tableIndex).children().first().html() + "?")) {
+  jQuery(document).on("click", "#remove" + tableIndex, function(e) {
+    if (confirm("Are you sure you want to remove " + jQuery("#row" + tableIndex).children().first().html() + "?")) {
       // emit the id of the row
       e.preventDefault();
-      socket.emit('remove_name', tableIndex, $("#row" + tableIndex).children().first().html());
+      socket.emit('remove_name', tableIndex, jQuery("#row" + tableIndex).children().first().html());
     }
   })
 
   if (tL !== "") {
     if (tB === "") { // on the water
-      $("#checkOutBtn" + tableIndex).before(tL);
-      $("#checkOutBtn" + tableIndex).hide();
-      $("#checkInBtn" + tableIndex).show();
+      jQuery("#checkOutBtn" + tableIndex).before(tL);
+      jQuery("#checkOutBtn" + tableIndex).hide();
+      jQuery("#checkInBtn" + tableIndex).show();
       setBtnListener("checkInBtn" + tableIndex, tableIndex, 'in');
     } else { // off the water
-      $("#checkOutBtn" + tableIndex).before(tL);
-      $("#checkOutBtn" + tableIndex).hide();
-      $("#checkInBtn" + tableIndex).before(tB);
-      $("#checkInBtn" + tableIndex).hide();
+      jQuery("#checkOutBtn" + tableIndex).before(tL);
+      jQuery("#checkOutBtn" + tableIndex).hide();
+      jQuery("#checkInBtn" + tableIndex).before(tB);
+      jQuery("#checkInBtn" + tableIndex).hide();
     }
   } else { // not yet on water
-    $("#checkInBtn" + tableIndex).hide();
+    jQuery("#checkInBtn" + tableIndex).hide();
     setBtnListener("checkOutBtn" + tableIndex, tableIndex, 'out');
     setBtnListener("checkInBtn" + tableIndex, tableIndex, 'in');
   }
@@ -75,26 +75,26 @@ socket.on('add_data', function (tableIndex, tL, tB) {
 
 // remove a name
 socket.on('remove_name', function(tableIndex) {
-  $("#row" + tableIndex).remove();
+  jQuery("#row" + tableIndex).remove();
 });
 
 // check out
 socket.on('check_out', function(tableIndex, time) {
-  $("#checkOutBtn" + tableIndex).before(time); // add check-out time
-  $("#checkOutBtn" + tableIndex).hide(); // hide the button
+  jQuery("#checkOutBtn" + tableIndex).before(time); // add check-out time
+  jQuery("#checkOutBtn" + tableIndex).hide(); // hide the button
   // show check-in button
-  $("#checkInBtn" + tableIndex).show();
+  jQuery("#checkInBtn" + tableIndex).show();
 });
 
 // check in
 socket.on('check_in', function(tableIndex, time) {
-  $("#checkInBtn" + tableIndex).before(time); // add check-in time
-  $("#checkInBtn" + tableIndex).hide(); // hide the button
+  jQuery("#checkInBtn" + tableIndex).before(time); // add check-in time
+  jQuery("#checkInBtn" + tableIndex).hide(); // hide the button
 });
 
 // set button listener (for code readability)
 function setBtnListener(id, tableIndex, inout) {
-  $(document).on("click", "#" + id, function(e) {
+  jQuery(document).on("click", "#" + id, function(e) {
     // get current time
     let today = new Date();
     let mins = today.getMinutes();
@@ -104,6 +104,6 @@ function setBtnListener(id, tableIndex, inout) {
 
     // emit the id of the check-out button, current time, and name
     e.preventDefault();
-    socket.emit('check_' + inout, tableIndex, time, $("#row" + tableIndex).children().first().html());
+    socket.emit('check_' + inout, tableIndex, time, jQuery("#row" + tableIndex).children().first().html());
   })
 }
