3b5776c52f2027abf0d08bc1404b2ab6a6ef34dc
Andrew Gasparovich <afgasparo@gmail.com>
2021-02-03 00:07:29 -0500

    Database complete for localhost. App has basic functionality.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const idx = require(__dirname + "/idx.js");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/namesDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/namesDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const namesSchema = {
+  id: Number,
   name: String,
   timeLeft: String,
   timeBack: String
@@ -19,7 +26,14 @@ const Name = mongoose.model("Name", namesSchema);
 
 // send index.ejs
 app.get("/", function(req, res) {
-  res.render("index.ejs");
+  Name.find({}, function(err, foundNames) {
+    console.log("foundNames.length = " + foundNames.length);
+    res.render("index", {newListItems: foundNames});
+    ids = []
+    foundNames.forEach(docs => ids.push(Number(docs.id)));
+    idx.setIdx(Math.max(...ids) + 1);
+    console.log("set idx to " + idx.getIdx());
+  })
 })
 
 // send index.js
@@ -28,57 +42,84 @@ app.get("/index.js", function(req, res) {
 });
 
 // open connection
-io.on('connection', function(socket) {
-  // catch emissions for functions
-  socket.on('new_name', function(data, tableIndex, sid) {
-    const name = new Name({
-      name: data
-    });
+io.on('connection', function (socket) {
 
-    Name.create(name, function(err){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Successfully saved " + data + " to namesDB");
+  socket.on('add_data', function() {
+    Name.find({}, function (err, found) {
+      for (let i = 0; i < found.length; i++) {
+        socket.emit('add_data', found[i].id, found[i].timeLeft, found[i].timeBack);
       }
     })
-
-    io.emit('new_name', "<td>" + data + "</td><td><button id='checkOutBtn" + tableIndex + "' class='btn btn-light'>Check Out</button></td><td></td><td><button id='remove" + tableIndex + "' class='btn btn-light'>X</button></td></tr>", sid);
   });
-  socket.on('remove_name', function(data, name2) {
-    Name.deleteOne({name: name2}, function(err){
+
+  socket.on('new_name', function(data) {
+    const name = new Name({
+      id: idx.getIdx(),
+      name: data,
+      timeLeft: "",
+      timeBack: ""
+    });
+
+    Name.find({name: data}, function (err, found) {
+      if (!err) {
+        if (found.length > 0) {
+          console.log(data + " already exists in namesDB");
+        }
+        else { // create document in namesDB
+          console.log("create() called");
+          Name.create(name, function(err) {
+            if (!err) {
+              console.log("Successfully saved " + idx.getIdx() + " " + data + " to namesDB");
+              io.emit('new_name', idx.getIdx(), "<td>" + data + "</td><td><button id='checkOutBtn" + idx.getIdx() + "' class='btn btn-light'>Check Out</button></td><td><button id='checkInBtn" + idx.getIdx() + "' class='btn btn-light'>Check In</button></td><td><button id='remove" + idx.getIdx() + "' class='btn btn-light'>X</button></td></tr>");
+              idx.incIdx();
+            } else {
+              console.log("error in creating new namesDB document" + err);
+            }
+          })
+        }
+      }
+    });
+  });
+
+  socket.on('remove_name', function(idx, name2) {
+    Name.deleteOne({
+      id: idx
+    }, function(err) {
       if (!err) {
         console.log("Successfully deleted " + name2 + " from namesDB");
       } else {
-        console.log(err);
+        console.log("error: " + err);
       }
-    })
-
-    io.emit('remove_name', data);
+    });
+    io.emit('remove_name', idx);
   });
 
-  socket.on('check_out', function(data, time, name2) {
-    Name.findOneAndUpdate({name: name2}, {timeLeft: time}, {upsert: true}, function(err) {
+  socket.on('check_out', function(idx, time, name2) {
+    Name.findOneAndUpdate({id: idx}, {timeLeft: time}, {upsert: true, useFindAndModify: false},
+      function(err) {
       if (!err) {
         console.log("Successfully added " + time + " to time-left for " + name2 + " in namesDB");
       } else {
-        console.log(err);
+        console.log("error: " + err);
       }
-    })
-
-    io.emit('check_out', data, time);
+    });
+    io.emit('check_out', idx, time);
   });
 
-  socket.on('check_in', function(data, time, name2) {
-    Name.findOneAndUpdate({name: name2}, {timeBack: time}, {upsert: true}, function(err) {
+  socket.on('check_in', function(idx, time, name2) {
+    Name.findOneAndUpdate({
+      id: idx }, {
+      timeBack: time }, {
+      upsert: true,
+      useFindAndModify: false
+    }, function(err) {
       if (!err) {
         console.log("Successfully added " + time + " to time-back for " + name2 + " in namesDB");
       } else {
-        console.log(err);
+        console.log("error: " + err);
       }
-    })
-
-    io.emit('check_in', data, time);
+    });
+    io.emit('check_in', idx, time);
   });
 });
 
--- index.js
+++ index.js
@@ -1,77 +1,108 @@
+// set up the socket
 var socket = io.connect('http://localhost:8080');
-
 socket.on("connect", () => {
   console.log("this socket ID = " + socket.id);
 });
 
-// Allows the buttons to work for the row they're on
-let tableIndex = 1;
+// reload all the times and buttons whenever the page is fully loaded
+$(window).on('load', function() {
+  var x = window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart + "ms";
+  console.log("page loaded in " + x);
+  socket.emit('add_data');
+})
 
 // submit new table item without reload
 $('#addNameForm').submit(function(e) {
   e.preventDefault(); // prevents page reloading
-  socket.emit('new_name', $('#nameBox').val(), tableIndex, socket.id);
+  socket.emit('new_name', $('#nameBox').val());
   $('#nameBox').val('');
   return false;
 });
 
 // append the new name
-socket.on('new_name', function(data, sid) {
+socket.on('new_name', function(tableIndex, data) {
   $('#tableBody').append($("<tr>").html(data)); // append data to table
   $('#tableBody tr:last').prop("id", "row" + tableIndex); // append row# id
 
-  // click listener for remove name button
+  // hide check-in button
+  $("#checkInBtn" + tableIndex).hide();
+
+  // click listener for remove button
   $(document).on("click", "#remove" + tableIndex, function(e) {
-    if (confirm("Are you sure you want to remove " + $(this).parent().parent().children().first().html() + "?")) {
+    if (confirm("Are you sure you want to remove " + $("#row" + tableIndex).children().first().html() + "?")) {
       // emit the id of the row
       e.preventDefault();
-      socket.emit('remove_name', $(this).parent().parent().prop("id"), $(this).parent().parent().children().first().html());
+      socket.emit('remove_name', tableIndex, $("#row" + tableIndex).children().first().html());
+    }
+  })
+  // click listener for check-out button
+  setBtnListener("checkOutBtn" + tableIndex, tableIndex, 'out');
+
+  // click listener for check-in button
+  setBtnListener("checkInBtn" + tableIndex, tableIndex, 'in');
+});
+
+// add data
+socket.on('add_data', function (tableIndex, tL, tB) {
+  // click listener for remove
+  $(document).on("click", "#remove" + tableIndex, function(e) {
+    if (confirm("Are you sure you want to remove " + $("#row" + tableIndex).children().first().html() + "?")) {
+      // emit the id of the row
+      e.preventDefault();
+      socket.emit('remove_name', tableIndex, $("#row" + tableIndex).children().first().html());
     }
   })
 
-  // click listener for check-out button
-  $(document).on("click", "#checkOutBtn" + tableIndex, function(e) {
-    // get current time
-    let today = new Date();
-    let mins = today.getMinutes();
-    if (mins.toString().length === 1)
-      mins = "0" + mins;
-    let time = today.getHours() + ":" + mins;
-    // emit the id of the check-out button and time
-    e.preventDefault();
-    socket.emit('check_out', $(this).prop("id"), time, $(this).parent().parent().children().first().html());
-  })
-  tableIndex++;
-});
+  if (tL !== "") {
+    if (tB === "") { // on the water
+      $("#checkOutBtn" + tableIndex).before(tL);
+      $("#checkOutBtn" + tableIndex).hide();
+      $("#checkInBtn" + tableIndex).show();
+      setBtnListener("checkInBtn" + tableIndex, tableIndex, 'in');
+    } else { // off the water
+      $("#checkOutBtn" + tableIndex).before(tL);
+      $("#checkOutBtn" + tableIndex).hide();
+      $("#checkInBtn" + tableIndex).before(tB);
+      $("#checkInBtn" + tableIndex).hide();
+    }
+  } else { // not yet on water
+    $("#checkInBtn" + tableIndex).hide();
+    setBtnListener("checkOutBtn" + tableIndex, tableIndex, 'out');
+    setBtnListener("checkInBtn" + tableIndex, tableIndex, 'in');
+  }
+})
 
 // remove a name
-socket.on('remove_name', function(data) {
-  $("#" + data).remove();
+socket.on('remove_name', function(tableIndex) {
+  $("#row" + tableIndex).remove();
 });
 
 // check out
-socket.on('check_out', function(data, time) {
-  $("#" + data).before(time); // add check-out time
-  $("#" + data).hide(); // hide the button
-  // add HTML to create check-in button (after checking out)
-  $("#" + data).parent().next().html("<button id='checkInBtn" + data.slice(-1) + "' class='btn btn-light'>Check In</button>");
+socket.on('check_out', function(tableIndex, time) {
+  $("#checkOutBtn" + tableIndex).before(time); // add check-out time
+  $("#checkOutBtn" + tableIndex).hide(); // hide the button
+  // show check-in button
+  $("#checkInBtn" + tableIndex).show();
+});
 
-  // add click listener for check-in button
-  $(document).on("click", "#checkInBtn" + data.slice(-1), function(e) {
+// check in
+socket.on('check_in', function(tableIndex, time) {
+  $("#checkInBtn" + tableIndex).before(time); // add check-in time
+  $("#checkInBtn" + tableIndex).hide(); // hide the button
+});
+
+// set button listener (for code readability)
+function setBtnListener(id, tableIndex, inout) {
+  $(document).on("click", "#" + id, function(e) {
     // get current time
     let today = new Date();
     let mins = today.getMinutes();
     if (mins.toString().length === 1)
       mins = "0" + mins;
     let time = today.getHours() + ":" + mins;
-    // emit the id of the check-in button and time
-    e.preventDefault();
-    socket.emit('check_in', "checkInBtn" + data.slice(-1), time, $("#row" + data.slice(-1)).children().first().html());
-  })
-});
 
-// check in
-socket.on('check_in', function(data, time) {
-  $("#" + data).before(time); // add check-in time
-  $("#" + data).hide(); // hide the button
-});
+    // emit the id of the check-out button, current time, and name
+    e.preventDefault();
+    socket.emit('check_' + inout, tableIndex, time, $("#row" + tableIndex).children().first().html());
+  })
+}
